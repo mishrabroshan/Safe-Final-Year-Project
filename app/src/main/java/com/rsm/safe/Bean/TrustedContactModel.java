5b1ae7065eb8b946acdf80e26083a74ec58ecdf7
Roshan Mishra <mishrabroshan@gmail.com>
2020-03-23 17:36:06 +0530

    Commit!

--- app/src/main/java/com/rsm/safe/Bean/TrustedContactModel.java
+++ app/src/main/java/com/rsm/safe/Bean/TrustedContactModel.java
@@ -1,4 +1,37 @@
 package com.rsm.safe.Bean;
 
 public class TrustedContactModel {
+    private int ID;
+    private String Name;
+    private String Number;
+
+    public TrustedContactModel(int ID, String name, String number) {
+        this.ID = ID;
+        Name = name;
+        Number = number;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getNumber() {
+        return Number;
+    }
+
+    public void setNumber(String number) {
+        Number = number;
+    }
 }
